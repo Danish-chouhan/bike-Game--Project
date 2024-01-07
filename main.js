2f99e0c94f499f6bcfe7c7712eb245cf1f7bbff3
Danish-chouhan <danishchouhan41@gmail.com>
2024-01-08 03:10:16 +0530

    some Functionalty remaiing but almost done

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ img.setAttribute("class","bikeImg")
 // ----container1
 
 const video = createAndAppend("video","src","https://media.istockphoto.com/id/472414271/video/rolling-street.mp4?s=mp4-640x640-is&k=20&c=3MNJlLytI5Ac-8qivr4vO2ySOQ3x7gslKH5ieMhZwzA=",container1)
-video.setAttribute("loop","")
-video.setAttribute("autoplay","")
 
 
 // ----container2
@@ -19,14 +17,14 @@ const controler = createAndAppend("div","class","controler",container2)
 // handel
 
 const btnContainerOfHandel = createAndAppend("div","class","buttonConteinerHandel",handelContainer)
-const btnLeft = createAndAppend("button","class","left",btnContainerOfHandel)
-const btnRight = createAndAppend("button","class","right",btnContainerOfHandel)
+const btnLeft = createAndAppend("button","class","left",btnContainerOfHandel,"click")
+const btnRight = createAndAppend("button","class","right",btnContainerOfHandel,"click")
 
 
 // controller
 const btnContainerOfController = createAndAppend("div","class","buttonContainerController",controler)
-const btnBreak = createAndAppend("button","class","break",btnContainerOfController)
-const btnAccelerator = createAndAppend("button","class","accelerator",btnContainerOfController)
+const btnBreak = createAndAppend("button","class","break",btnContainerOfController,"click")
+const btnAccelerator = createAndAppend("button","class","accelerator",btnContainerOfController,"click")
 
 function createAndAppend(tag, attType, attName, parent, event) {
     const element = document.createElement(tag);
@@ -36,8 +34,36 @@ function createAndAppend(tag, attType, attName, parent, event) {
   if (!!parent) {
    parent.append(element)  
 }
+
+let translationValue = 0;
   if (!!event) {
-   
+   element.addEventListener(event,(e)=>{
+    if(!!(e.type === "click")){
+      if(e.target.classList[0] === "right"){
+        translationValue += 15;
+        console.log(translationValue);
+        if(translationValue < 135){
+          img.style.transform = `translate(${translationValue}%,${1}%)`
+        }
+      }
+      if(e.target.classList[0] === "left"){
+        translationValue -= -15; 
+        console.log(translationValue);
+        if(translationValue > -140){
+          img.style.transform = `translate(${translationValue}%,-${1}%)`
+        }
+      }
+      if(e.target.classList[0] === "accelerator"){
+        video.play()
+        // video.playbackRate = 1;
+        video.setAttribute("loop","")
+        video.setAttribute("autoplay","")
+      }
+     if(e.target.classList[0] === "break"){
+     video.pause()
+    }
+    }
+   })
   }
   return element
 }
